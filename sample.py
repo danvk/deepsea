a6d4ead47d2fb556579c86bb81f11f60e6803a7d
Dan Vanderkam <danvdk@gmail.com>
2016-01-21 16:30:08 -0500

    report progress as sampling happens

--- sample.py
+++ sample.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
-'''Extract a random sample from an HDF5 file.'''
+'''Extract a random sample from an HDF5 file.
+
+Usage:
+    ./sample.py in.mat 12345 out.mat
+'''
 
 import sys
 import h5py
 import numpy as np
 import random
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
 
 def extract_sample(dataset, count, outset):
     sets = dataset.keys()
@@ -16,11 +27,19 @@ def extract_sample(dataset, count, outset):
     indices = sorted(random.sample(xrange(num), count))
 
     for name, dataset in dataset.iteritems():
-        if len(dataset.shape) == 2:
-            d = dataset[:,indices]
-        else:
-            d = dataset[:,:,indices]
-        outset.create_dataset(name, data=d)
+        shape = list(dataset.shape)
+        shape[-1] = count
+        print shape
+
+        o = outset.create_dataset(name, shape, dtype=dataset.dtype)
+
+        b = 100  # chunk size
+        for n, idxs in enumerate(chunks(indices, b)):
+            if len(dataset.shape) == 2:
+                o[:,n*b:(n+1)*b] = dataset[:,idxs]
+            else:
+                o[:,:,n*b:(n+1)*b] = dataset[:,:,idxs]
+            print '%s %d/%d...' % (name, n * b, count)
 
 
 if __name__ == '__main__':
